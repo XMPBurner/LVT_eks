ffa8e78481cc00d7789d9f55d7fd315fcf06e048
unknown <emilsporikis2@gmail.com>
2023-04-04 16:32:27 +0300

    Klienta loga iesākšana

--- WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
+++ WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/logo.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/logo1.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logo.png")]
 
 
